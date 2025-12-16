e7d88ee3fc6192b0e524c5891fa9e339dcd4d582
TugceOnder <tugceonder775@gmail.com>
2025-12-16 16:03:36 +0300

    django projesi oluşturuldu

---
+++ catalog/catalog/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('', include('pages.urls')),
+    path('movies/', include('movies.urls')),
+    path('user/', include('user.urls')),
+    path('admin/', admin.site.urls),
+]
